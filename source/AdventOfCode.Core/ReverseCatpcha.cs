f43259ad88ed086c45ab59b229941f680c10a4a2
Nuno Costa <ncosta@equalexperts.com>
2017-12-21 17:49:10 +0000

    ADD : day 1

---
+++ source/AdventOfCode.Core/ReverseCatpcha.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AdventOfCode.Core
+{
+    public class ReverseCatpcha
+    {
+        public int Solve(string sequence)
+        {
+            var sequenceArray = sequence.ToCharArray();
+            var sum = 0;
+
+            for (int i = 0; i < sequenceArray.Length; i++)
+            {
+                var current = Convert.ToInt32(sequenceArray[i].ToString());
+
+                var nextIndex = i + 1;
+
+                if(nextIndex >= sequenceArray.Length)
+                {
+                    nextIndex = 0;
+                }
+
+                var next = Convert.ToInt32(sequenceArray[nextIndex].ToString());
+                
+                if(current == next)
+                {
+                    sum += current;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
